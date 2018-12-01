27e22849bd437f7bf9c58f1bf74cf49e445b582b
cliang <liangchao827@gmail.com>
2018-12-01 13:53:21 -0600

    revise bugs in result mat file and figure presentation

--- method/script/mmap_pc.bat
+++ method/script/mmap_pc.bat
@@ -2,4 +2,4 @@ cd ..\dafe\
 set debug_flag=0
 set run_mode="'parallel'"
 start matlab.exe -nodesktop -nosplash -r ^
-"test_dafe(%script_flag%,%debug_flag%,%run_mode%); quit"
+"test_dafe(%debug_flag%,%run_mode%); quit"
--- method/script/whuhpc.sh
+++ method/script/whuhpc.sh
@@ -4,4 +4,4 @@ module load matlab/R2017b
 debug_flag=0
 run_mode="'parallel'"
 matlab -nodesktop -nosplash -nodisplay -r \
-"test_dafe($debug_flag, $run_mode); quit"
+"test_dafe($debug_flag,$run_mode); quit"
